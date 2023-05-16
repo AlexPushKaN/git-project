91832835e54e573508b01d466f4cfb092b901069
Александр Муклинов <dkan425@gmail.com>
2023-05-16 17:41:23 +0400

    deleded line

--- git-project/ViewController.swift
+++ git-project/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("What's up guys!!!")
-        print("Cool!!!")
-        print("Yes!!!")
+
     }
 }
 
