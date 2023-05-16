bb291e6b090a0ed0211b267f2b4a934b5c8b52ab
Александр Муклинов <dkan425@gmail.com>
2023-05-16 17:39:39 +0400

    delete print

--- git-project/ViewController.swift
+++ git-project/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hi, guys and girls!")
         print("What's up guys!!!")
         print("Cool!!!")
         print("Yes!!!")
