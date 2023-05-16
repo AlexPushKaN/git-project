1862b5f027eff86e7ff50cf9c2603823972eeaea
Alex <119400523+AlexPushKaN@users.noreply.github.com>
2023-05-16 17:51:46 +0400

    Update ViewController.swift

--- git-project/ViewController.swift
+++ git-project/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("What's up guys!!!")
-
+        print("What's up guys!!!")
     }
 }
 
