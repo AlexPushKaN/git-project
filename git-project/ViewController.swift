7fcf1231f5059f2d155082411379ebbf6c68c07a
Александр Муклинов <dkan425@gmail.com>
2023-05-16 17:01:39 +0400

    added line print "Cool!"

--- git-project/ViewController.swift
+++ git-project/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hi, guys and girls!")
         print("What's up guys!!!")
+        print("Cool!!!")
     }
 }
 
