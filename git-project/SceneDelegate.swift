6efd764776d264c631b640b4568a0d8908ca08a9
Александр Муклинов <dkan425@gmail.com>
2023-05-17 14:56:07 +0400

    deleted line print

--- git-project/SceneDelegate.swift
+++ git-project/SceneDelegate.swift
@@ -19,6 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
+        print("Yes!!!")
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
